ea28a48e0d040e49d7224cc1f980fd7b86f7c525
Cocaadm <andreperezcastelo@protonmail.ch>
2023-08-18 18:16:25 -0300

    template home

--- App/views.py
+++ App/views.py
@@ -2,14 +2,7 @@ from django.shortcuts import render
 from .models import Posts
 
 def home(request):
-    posts = Posts.objects.all()
-    last_post = Posts.objects.order_by('-date_creat').first()
-    ##recent_posts = Posts.objects.order_by('-date_creat')
-    if last_post:
-        previous_post = Posts.objects.filter(date_creat__lt=last_post.date_creat).order_by('-date_creat').first()
-    else:
-        previous_post = None
-    
-    return render(request, 'home.html', {'posts': posts, 'last_post': last_post, 'previous_post': previous_post})
+    posts = Posts.objects.all().order_by('-date_creat')
+    return render(request, 'home.html', {'posts' : posts})
 
 
