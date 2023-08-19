dccf18ae86cc6d825bc40d46f951da9a86685e9b
Cocaadm <andreperezcastelo@protonmail.ch>
2023-08-18 22:15:47 -0300

    date and translates

--- mainblog/settings.py
+++ mainblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,9 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-nr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
+DATE_FORMAT = 'd/m/Y'
+TIME_FORMAT = 'H:i'
+
+os.path.join(BASE_DIR, 'locale'),
 
 USE_I18N = True
 
