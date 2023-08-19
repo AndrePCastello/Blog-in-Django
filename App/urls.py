d0636d1f42bc9dac6a756ace304d2568e67cd148
Cocaadm <andreperezcastelo@protonmail.ch>
2023-08-19 01:23:59 -0300

    finished

--- App/urls.py
+++ App/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('posts/for-date/<str:date>', views.posts_for_date, name='posts_for_date')
+    path('posts/for-date/<str:date>', views.posts_for_date, name='posts_for_date'),
+    path('details_post/ <int:id_post>', views.details_post, name='details_post')
 ]
 
--- App/views.py
+++ App/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Posts
 
 def home(request):
@@ -9,3 +9,8 @@ def home(request):
 def posts_for_date(request, date):
     posts = Posts.objects.filter(date_creat__date=date).order_by('-date_creat')
     return render (request, 'posts-for-date.html', {'posts' : posts, 'data' : date})
+
+
+def details_post(request, id_post):
+    post = Posts.objects.get(pk=id_post)
+    return render (request, 'details-for-post.html', {'post' : post})
